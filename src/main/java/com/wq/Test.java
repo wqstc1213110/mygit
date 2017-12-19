32c037eb3267802301f7e68f50cbbeb9e3e4befc
wq <1291319407@qq.com>
2017-12-19 20:49:52 +0800

    "绝对终极版"

--- src/main/java/com/wq/Test.java
+++ src/main/java/com/wq/Test.java
@@ -7,8 +7,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("11111111111111");
         System.out.println("222222222222222");
-        System.out.println("333333333333333");
-        System.out.println("5555555555555");
-        System.out.println("6666666666666666");
+
+
     }
 }
